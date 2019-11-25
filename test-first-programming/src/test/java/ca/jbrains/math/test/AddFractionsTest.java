9b0936cdf1f1bee1c7bb788db530aa21f28df9ac
J. B. Rainsberger <me@jbrains.ca>
2019-11-25 12:45:31 +0100

    We can add any two fractions, as long as they are integers.

--- test-first-programming/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ test-first-programming/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -22,6 +22,12 @@ public class AddFractionsTest {
         Assertions.assertEquals(7, sum.intValue());
     }
 
+    @Test
+    void notZeroPlusNotZero() throws Exception {
+        Fraction sum = new Fraction(5).plus(new Fraction(8));
+        Assertions.assertEquals(13, sum.intValue());
+    }
+
     public static class Fraction {
         private final int integerValue;
 
@@ -32,8 +38,10 @@ public class AddFractionsTest {
         public Fraction plus(Fraction that) {
             if (that.integerValue == 0)
                 return this;
-            else
+            else if (this.integerValue == 0)
                 return that;
+            else
+                return new Fraction(this.integerValue + that.integerValue);
         }
 
         public int intValue() {

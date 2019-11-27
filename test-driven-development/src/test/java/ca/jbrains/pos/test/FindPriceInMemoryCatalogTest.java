53d2b48e7103d6e635285ddbb26395b38eb32efe
J. B. Rainsberger <me@jbrains.ca>
2019-11-27 10:36:11 +0100

    [FAILING TEST] We describe how to find the price for a barcode with an in-memory implementation of Catalog.

---
+++ test-driven-development/src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -0,0 +1,29 @@
+package ca.jbrains.pos.test;
+
+import io.vavr.control.Option;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindPriceInMemoryCatalogTest {
+    @Test
+    void productFound() throws Exception {
+        final SellOneItemControllerTest.Price matchingPrice = SellOneItemControllerTest.Price.cents(795);
+        Assertions.assertEquals(Option.of(matchingPrice), new InMemoryCatalog(
+                new HashMap<>() {{
+                    put("12345", matchingPrice);
+                }}
+        ).findPrice("12345"));
+    }
+
+    private static class InMemoryCatalog {
+        public InMemoryCatalog(Map<String, SellOneItemControllerTest.Price> pricesByBarcode) {
+        }
+
+        public Option<SellOneItemControllerTest.Price> findPrice(String barcode) {
+            return Option.of(new SellOneItemControllerTest.Price());
+        }
+    }
+}

98c84576055cc890ca5d1eaa74557b72b8991386
J. B. Rainsberger <me@jbrains.ca>
2019-11-27 14:16:27 +0100

    Write a test as a parmaterized test in order to make it easier to add more examples.

--- test-driven-development/src/test/java/ca/jbrains/pos/test/FormatPriceTest.java
+++ test-driven-development/src/test/java/ca/jbrains/pos/test/FormatPriceTest.java
@@ -1,11 +1,24 @@
 package ca.jbrains.pos.test;
 
+import net.jqwik.api.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FormatPriceTest {
-    @Test
-    void happyPath() throws Exception {
-        Assertions.assertEquals("14,97\u00a0€", EnglishLanguageSlovakNumberFormat.formatPrice(Price.cents(1497)));
+    @Data
+    Iterable<Tuple.Tuple2<Price, String>> examples() {
+        return Table.of(
+                Tuple.of(
+                        Price.cents(1497), "14,97"
+                )
+        );
+    }
+
+    @Property
+    @FromData("examples")
+    void happyPath(@ForAll Price price, @ForAll String expectedFormat) throws Exception {
+        Assertions.assertEquals(
+                String.format("%s\u00a0€", expectedFormat),
+                EnglishLanguageSlovakNumberFormat.formatPrice(price));
     }
 }

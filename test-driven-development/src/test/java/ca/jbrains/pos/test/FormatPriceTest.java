4f0356b50f72c41267015a73d3c4c99899cf94ca
J. B. Rainsberger <me@jbrains.ca>
2019-11-27 14:19:37 +0100

    Confirm that formatPrice() formats all the prices that we care about.

--- test-driven-development/src/test/java/ca/jbrains/pos/test/FormatPriceTest.java
+++ test-driven-development/src/test/java/ca/jbrains/pos/test/FormatPriceTest.java
@@ -1,16 +1,19 @@
 package ca.jbrains.pos.test;
 
 import net.jqwik.api.*;
+import net.jqwik.api.Tuple.Tuple2;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 public class FormatPriceTest {
     @Data
-    Iterable<Tuple.Tuple2<Price, String>> examples() {
+    Iterable<Tuple2<Price, String>> examples() {
         return Table.of(
-                Tuple.of(
-                        Price.cents(1497), "14,97"
-                )
+                Tuple.of(Price.cents(1497), "14,97"),
+                Tuple.of(Price.cents(0), "0,00"),
+                Tuple.of(Price.cents(1), "0,01"),
+                Tuple.of(Price.cents(40), "0,40"),
+                Tuple.of(Price.cents(68), "0,68"),
+                Tuple.of(Price.cents(8237611), "82376,11")
         );
     }
 
